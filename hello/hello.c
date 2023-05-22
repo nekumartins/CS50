e3ac0f918f4acfa334a7ba6fe9e09a6f88f4c6d1
Neku <akpotohwoo@gmail.com>
2023-05-22 18:33:24 +0100

    added an hello function

--- hello/hello.c
+++ hello/hello.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
+void sayhello(char * name, int num);
 int main(void)
-{
-    printf("Hello, Reader");
+{   char name[20];
+    printf("What is your name: ");
+    scanf("%s",name);
+    int num; 
+    printf("Hello %s, enter a random number:) : ", name);
+    scanf("%d", &num);
+    sayhello(name, num);
+
+
+    
 }
 
-void sayhello(int num)
+void sayhello(char * name, int num)
 {
     for (int i = 0; i < num; i++)
     {
-        
+        printf("Hello, %s\n", name);
     }
 }
