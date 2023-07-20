8e6aca08b1536f70db3c2915ce82106eaebc3e06
Riley <akpotohwoo@gmail.com>
2023-07-20 15:53:43 +0100

    Add entry

--- web/wiki/encyclopedia/views.py
+++ web/wiki/encyclopedia/views.py
@@ -2,9 +2,32 @@ from django.shortcuts import render
 
 from . import util
 
+from markdown import Markdown
+
+
+def convert(title):
+    md = Markdown()
+    if title :
+        return md.convert(title)
+    else :
+        return None
+    
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
+ 
+def title(request, TITLE):
+    content = convert(util.get_entry(TITLE))
+    entry = TITLE
+    if content :
+        return render(request, "encyclopedia/titles.html", {
+        "entry" : entry,
+        "entries" : content
+        })
+    else :
+        return render(request,"encyclopedia/errors.html",{
+            "entry": entry,
+        } )
